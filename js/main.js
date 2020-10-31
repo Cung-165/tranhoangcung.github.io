05bf960ffac91a0be03fb38409dc3dce40c60179
Cung-165 <57405609+Cung-165@users.noreply.github.com>
2020-11-01 00:27:45 +0700

    Project Icon club

---
+++ js/main.js
@@ -0,0 +1,92 @@
+window.onload = function() {
+    animateSequence();
+    animateRandom();
+};
+
+var pages = document.getElementsByClassName('page');
+for(var i = 0; i < pages.length; i++)
+  {
+    var page = pages[i];
+    if (i % 2 === 0)
+      {
+        page.style.zIndex = (pages.length - i);
+      }
+  }
+
+document.addEventListener('DOMContentLoaded', function(){
+  for(var i = 0; i < pages.length; i++)
+    {
+      //Or var page = pages[i];
+      pages[i].pageNum = i + 1;
+      pages[i].onclick=function()
+        {
+          if (this.pageNum % 2 === 0)
+            {
+              this.classList.remove('flipped');
+              this.previousElementSibling.classList.remove('flipped');
+            }
+          else
+            {
+              this.classList.add('flipped');
+              this.nextElementSibling.classList.add('flipped');
+            }
+         }
+      }
+})
+
+
+
+function animateSequence() {
+    var a = document.getElementsByClassName('sequence');
+    for (var i = 0; i < a.length; i++) {
+        var $this = a[i];
+        var letter = $this.innerHTML;
+        letter = letter.trim();
+        var str = '';
+        var delay = 100;
+        for (l = 0; l < letter.length; l++) {
+            if (letter[l] != ' ') {
+                str += '<span style="animation-delay:' + delay + 'ms; -moz-animation-delay:' + delay + 'ms; -webkit-animation-delay:' + delay + 'ms; ">' + letter[l] + '</span>';
+                delay += 150;
+            } else
+                str += letter[l];
+        }
+        $this.innerHTML = str;
+    }
+}
+
+function animateRandom() {
+    var a = document.getElementsByClassName('random');
+    for (var i = 0; i < a.length; i++) {
+        var $this = a[i];
+        var letter = $this.innerHTML;
+        letter = letter.trim();
+        var delay = 70;
+        var delayArray = new Array;
+        var randLetter = new Array;
+        for (j = 0; j < letter.length; j++) {
+            while (1) {
+                var random = getRandomInt(0, (letter.length - 1));
+                if (delayArray.indexOf(random) == -1)
+                    break;
+            }
+            delayArray[j] = random;
+        }
+        for (l = 0; l < delayArray.length; l++) {
+            var str = '';
+            var index = delayArray[l];
+            if (letter[index] != ' ') {
+                str = '<span style="animation-delay:' + delay + 'ms; -moz-animation-delay:' + delay + 'ms; -webkit-animation-delay:' + delay + 'ms; ">' + letter[index] + '</span>';
+                randLetter[index] = str;
+            } else
+                randLetter[index] = letter[index];
+            delay += 80;
+        }
+        randLetter = randLetter.join("");
+        $this.innerHTML = randLetter;
+    }
+}
+
+function getRandomInt(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
